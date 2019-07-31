d03e7a8cda0486c2e120406828cc16562a609f9b
MixLeNain <mchretien@linuxmail.org>
2019-07-31 20:23:14 +0200

    Add basic NASM makefile

---
+++ NASM-MAKE.mk
@@ -0,0 +1,29 @@
+# Makefile for NASM programs
+# v1.0
+# Made by Maxime Chretien (MixLeNain)
+
+ASM=nasm
+ASMFLAGS=-f elf
+DEBUGFLAGS=-g -F stabs
+LD=ld
+LDFLAGS=-m elf_i386
+TARGET=main
+SRC=$(wildcard *.asm)
+OBJ=$(SRC:.asm=.o)
+
+.PHONY: all clean mrproper
+
+all: $(TARGET)
+
+$(TARGET) : $(OBJ)
+	$(LD) $(LDFLAGS) -o $@ $^
+
+%.o:%.asm
+# Add $(DEBUGFLAGS) at the end for debugging
+	$(ASM) -o $@ $< $(ASMFLAGS)
+
+clean:
+	rm -f *.o core
+
+mrproper: clean
+	rm -f $(TARGET)
